da5a4ef315469849bb237efec519cd19164f1455
liujia02 <liujia02@renrenche.com>
2018-09-25 19:10:19 +0800

    add twoSum

---
+++ algorithms/1/index.ts
@@ -0,0 +1,3 @@
+import {twoSum} from './two.sum';
+
+console.log(twoSum([2, 7, 11, 15], 9));
---
+++ algorithms/1/two.sum.ts
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+export const twoSum = (nums: number[], target: number): number => {
+  return target;
+};
