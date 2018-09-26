a54fff76692f67b3b1794ebe3f3fe35d030b827b
liujia02 <liujia02@renrenche.com>
2018-09-26 12:55:47 +0800

    1

--- algorithms/1/two.sum.ts
+++ algorithms/1/two.sum.ts
@@ -3,6 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-export const twoSum = (nums: number[], target: number): number => {
-  return target;
+export const twoSum = (nums: number[], target: number): number[] => {
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    const item = nums[i];
+    const other = target - item;
+    if (typeof obj[other] === 'number') {
+      return [obj[other], i];
+    } else {
+      obj[item] = i;
+    }
+  }
+  return [];
 };
