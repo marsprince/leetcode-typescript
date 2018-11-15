9296992b3f911e0866ac2717b7a0c9feba666957
liujia02 <liujia02@renrenche.com>
2018-11-15 16:33:25 +0800

    toMap

--- base/array.toMap.ts
+++ base/array.toMap.ts
@@ -1,15 +1,39 @@
-// 将数组映射为等效的value: index的object
-// 如果有相同的value，则返回Index列表
-export class Map {
-  constructor() {
+export class ArrayMap {
+  public map: Map<any, number>;
 
+  constructor() {
+    this.map = new Map<any, number>();
   }
 
-  get(value: any): number | number[] {
+  get(key: any): number | number[] {
+    return this.map.get(JSON.stringify(key));
+  }
 
+  set(key: any, value: any): void {
+    this.map.set(JSON.stringify(key), value);
   }
 }
 
-export const arrayToMap = (arr) => {
-
+// 将数组映射为等效的value: index的object
+// 如果有相同的value，则返回Index列表
+export const arrayToMap = (arr): ArrayMap => {
+  const result = new ArrayMap();
+  arr.forEach((value, index) => {
+    result.set(value, index);
+  });
+  return result;
 };
+
+const test1 = {
+  1: 'test',
+};
+const _arr = [
+  test1,
+  [{
+    2: 'test',
+  }],
+];
+
+const _arrToMap = arrayToMap(_arr);
+
+console.log(_arrToMap.get(test1));
